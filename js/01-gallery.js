0730c190420bb0a70fdf703b1a984fd60e36ecec
Vashchenia Leonid <dcs.lvv.87@gmail.com>
2022-09-22 21:37:42 +0300

    correction-01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,72 +1,7 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-// console.log(galleryItems);
-// const galleryRef = document.querySelector(".gallery");
-// console.log(galleryRef);
-
-// function createGalleryMarkup(items) {
-//   return items
-//     .map(({ preview, original, description }) => {
-//       return `<div class="gallery__item">
-//                 <a class="gallery__link" href=${original}
-//                     target="_blank" rel="noreferrer noopener">
-//                   <img
-//                     class="gallery__image"
-//                     src=${preview}
-//                     data-source=${original}
-//                     alt=${description}
-//                   />
-//                 </a>
-//               </div>`;
-//     })
-//     .join("");
-// }
-
-// galleryRef.insertAdjacentHTML("beforeend", createGalleryMarkup(galleryItems));
-
-// galleryRef.addEventListener("click", onGalleryClick);
-
-// const basicLightboxInstance = {
-//   instance: null,
-//   create(imgSource) {
-//     this.instance = basicLightbox.create(
-//       `
-//     <img src="${imgSource}" width="800" height="600">
-// `,
-//       {
-//         onShow: (instance) => {
-//           document.addEventListener("keydown", onEscapeKeyDown);
-//         },
-//         onClose: (instance) => {
-//           document.removeEventListener("keydown", onEscapeKeyDown);
-//         },
-//       }
-//     );
-//     this.instance.show();
-//   },
-//   destroy() {
-//     this.instance?.close();
-//   },
-// };
-
-// function onGalleryClick(event) {
-//   event.preventDefault();
-//   if (!event.target.classList.contains("gallery__image")) {
-//     return;
-//   }
-
-//   basicLightboxInstance.create(event.target.dataset.source);
-// }
-
-// function onEscapeKeyDown(event) {
-//   if (event.code !== "Escape") {
-//     return;
-//   }
-//   basicLightboxInstance.destroy();
-// }
 
 const galleryRef = document.querySelector(".gallery");
-console.log(galleryRef);
 
 function createGalleryMarkup(items) {
   return items
