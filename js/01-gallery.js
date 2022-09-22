1b9a9973a5ff4df01a294821a8dde04504934964
Vashchenia Leonid <dcs.lvv.87@gmail.com>
2022-09-22 21:36:01 +0300

    correction-01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -65,10 +65,48 @@ import { galleryItems } from "./gallery-items.js";
 //   basicLightboxInstance.destroy();
 // }
 
+const galleryRef = document.querySelector(".gallery");
+console.log(galleryRef);
+
 function createGalleryMarkup(items) {
-  return items.map((item) => {
-    console.log("ghb");
-  });
+  return items
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+                <a class="gallery__link" href=${original}
+                    target="_blank" rel="noreferrer noopener">
+                  <img
+                    class="gallery__image"
+                    src=${preview}
+                    data-source=${original}
+                    alt=${description}
+                  />
+                </a>
+              </div>`;
+    })
+    .join("");
 }
 
-createGalleryMarkup(galleryItems);
+galleryRef.insertAdjacentHTML("beforeend", createGalleryMarkup(galleryItems));
+
+function showModal(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`);
+
+  instance.show();
+
+  function onEscKeydown(event) {
+    if (event.code !== "Escape") {
+      return;
+    }
+    instance.close();
+    document.removeEventListener("keydown", onEscKeydown);
+  }
+  document.addEventListener("keydown", onEscKeydown);
+}
+
+galleryRef.addEventListener("click", showModal);
